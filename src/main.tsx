279d3c16e7f65d94459d7be8676b21f2539bea6a
pablosanchez <pablosanchezperianez83@gmail.com>
2024-05-24 23:03:56 +0200

    feat: routing and components added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.tsx'
+import Login from './components/login/Login'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Navbar from './components/shared/Navbar.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <RouterProvider router={router} />
+    {/* FOOTER */}
   </React.StrictMode>,
 )
