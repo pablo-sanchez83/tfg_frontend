7693a45a0ce07b6dbf7498bf23c2ce20fae71c41
Pablo Sanchez <pablosanchezperianez83@gmail.com>
2024-05-26 22:52:36 +0200

    feat: updated interfaces

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -13,3 +13,22 @@ export interface AuthResponse {
     token: string;
     user: User;
 }
+export interface Categoria_Culinaria {
+    id: number;
+    nombre: string;
+    descripcion: string;
+}
+export interface Empresas {
+    id : number;
+    nombre : string;
+    confirmado : boolean;
+    usuario : number;
+    localNum : number;
+}
+export interface Locales {
+    id : number;
+    usuario : User;
+    direccion : string;
+    categoria_culinaria : Categoria_Culinaria;
+    empresa : Empresas;
+}
