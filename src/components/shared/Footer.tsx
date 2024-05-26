057b040e68a7174832e4be4bab15b7b8b2444ca3
Pablo Sanchez <pablosanchezperianez83@gmail.com>
2024-05-26 22:52:55 +0200

    feat: updated footer and navbar

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer () {
     return (
-        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content justify-self-end w-full m-0">
             <aside>
-                <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+                <p className="text-xl text-center">Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
             </aside>
         </footer>
     )
