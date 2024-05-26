02c3eb8e485721e7e1bc27249723ddfd124fcc83
Pablo Sanchez <pablosanchezperianez83@gmail.com>
2024-05-26 22:52:14 +0200

    feat: updated genericLayout

--- src/assets/GenericLayout.tsx
+++ src/assets/GenericLayout.tsx
@@ -5,9 +5,11 @@ import Footer from '@/components/shared/Footer';
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Navbar />
-      {children}
-      <Footer />
+      <div className='min-h-screen m-0 overflow-x-hidden'>
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
     </>
   );
 };
