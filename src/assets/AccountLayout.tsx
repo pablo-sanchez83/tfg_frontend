aa8a845cd4580c8edf05e547d1df1e9a0c80ce12
Pablo Sanchez <pablo.sanchez@dialogotecnologico.com>
2024-06-08 18:24:28 +0200

    fix: overflow x hidden removed

--- src/assets/AccountLayout.tsx
+++ src/assets/AccountLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "@/components/shared/Footer";
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen m-0 relative grid grid-rows-[1fr_auto] overflow-x-hidden w-full justify-center">
+    <div className="min-h-screen m-0 relative grid grid-rows-[1fr_auto] w-full justify-center">
       <main className="mb-10">{children}</main>
       <Footer />
     </div>
--- src/assets/GenericLayout.tsx
+++ src/assets/GenericLayout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/shared/Footer";
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="min-h-screen m-0 relative grid grid-rows-[auto_1fr_auto] overflow-x-hidden">
+      <div className="min-h-screen m-0 relative grid grid-rows-[auto_1fr_auto]">
         <Header />
         <main className="w-full flex justify-center">{children}</main>
         <Footer />
