b54ba9281eb753120b948fc31f10ad2afa5f914c
Pablo Sanchez <pablo.sanchez@dialogotecnologico.com>
2024-06-08 17:58:47 +0200

    fix: no responsive form fixed

--- src/components/account/Cliente/Account.tsx
+++ src/components/account/Cliente/Account.tsx
@@ -15,15 +15,12 @@ import { Input } from "@/components/ui/input";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import styles from "./account.module.css";
-
 import { Link, useNavigate } from "react-router-dom";
 
 import { Contexto } from "@/components/Auth/AuthContext";
 import env from "@/lib/env";
 
 export default function Login() {
-  const [isSignUp, setIsSignUp] = useState(false);
   const [countryCode, setCountryCode] = useState("34");
   const navigate = useNavigate();
   const context = useContext(Contexto);
@@ -120,218 +117,174 @@ export default function Login() {
   return (
     <>
       <ToastContainer />
-      <div className="w-screen max-h-screen mt-10 grid place-items-center">
-        <div className={`${styles.container} ${isSignUp ? styles.active : ""}`}>
-          <div className={`${styles["form-container"]} ${styles["sign-up"]}`}>
-            <Form {...signUpForm}>
-              <form
-                onSubmit={signUpForm.handleSubmit(onSignUp)}
-                className="flex flex-col gap-4 items-center justify-center py-2 h-full"
-              >
-                <h1>
-                  <b>Crear Cuenta</b>
-                </h1>
-                <FormField
-                  control={signUpForm.control}
-                  name="username"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input placeholder="Nombre" {...field} />
-                      </FormControl>
-                      {signUpForm.formState.errors.username && (
-                        <p className="text-red-500">
-                          {signUpForm.formState.errors.username.message}
-                        </p>
-                      )}
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={signUpForm.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input placeholder="Email" {...field} />
-                      </FormControl>
-                      {signUpForm.formState.errors.email && (
-                        <p className="text-red-500">
-                          {signUpForm.formState.errors.email.message}
-                        </p>
-                      )}
-                    </FormItem>
-                  )}
-                />
-                <div className="flex gap-1 items-center">
-                  <CountryCodeSelect
-                    defaultValue={"34"}
-                    onValueChange={setCountryCode}
-                  />
-                  <FormField
-                    control={signUpForm.control}
-                    name="tel"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <Input
-                            className="w-1/3"
-                            placeholder="Teléfono"
-                            {...field}
-                          />
-                        </FormControl>
-                        {signUpForm.formState.errors.tel && (
-                          <p className="text-red-500">
-                            {signUpForm.formState.errors.tel.message}
-                          </p>
-                        )}
-                      </FormItem>
+      <div className="w-screen max-h-screen mt-10 gap-2 grid place-items-center">
+        <div className="flex flex-col md:flex-row justify-center gap-2">
+          <Form {...signUpForm}>
+            <form
+              onSubmit={signUpForm.handleSubmit(onSignUp)}
+              className="m-10 bg-white p-10 border rounded-md shadow-md flex flex-col gap-4 items-center justify-center py-2"
+            >
+              <h1>
+                <b>Crear Cuenta</b>
+              </h1>
+              <FormField
+                control={signUpForm.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Nombre" {...field} />
+                    </FormControl>
+                    {signUpForm.formState.errors.username && (
+                      <p className="text-red-500">
+                        {signUpForm.formState.errors.username.message}
+                      </p>
                     )}
-                  />
-                </div>
-                <FormField
-                  control={signUpForm.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input
-                          type="password"
-                          placeholder="Contraseña"
-                          {...field}
-                        />
-                      </FormControl>
-                      {signUpForm.formState.errors.password && (
-                        <p className="text-red-500">
-                          {signUpForm.formState.errors.password.message}
-                        </p>
-                      )}
-                    </FormItem>
-                  )}
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={signUpForm.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} />
+                    </FormControl>
+                    {signUpForm.formState.errors.email && (
+                      <p className="text-red-500">
+                        {signUpForm.formState.errors.email.message}
+                      </p>
+                    )}
+                  </FormItem>
+                )}
+              />
+              <div className="flex gap-1 items-center w-full">
+                <CountryCodeSelect
+                  defaultValue={"34"}
+                  onValueChange={setCountryCode}
                 />
                 <FormField
                   control={signUpForm.control}
-                  name="confirm_password"
+                  name="tel"
                   render={({ field }) => (
-                    <FormItem>
+                    <FormItem className="w-full">
                       <FormControl>
-                        <Input
-                          type="password"
-                          placeholder="Confirmar Contraseña"
-                          {...field}
-                        />
+                        <Input placeholder="Teléfono" {...field} />
                       </FormControl>
-                      {signUpForm.formState.errors.confirm_password && (
+                      {signUpForm.formState.errors.tel && (
                         <p className="text-red-500">
-                          {signUpForm.formState.errors.confirm_password.message}
+                          {signUpForm.formState.errors.tel.message}
                         </p>
                       )}
                     </FormItem>
                   )}
                 />
-                <Button
-                  type="submit"
-                  className="w-1/2 self-center bg-[#E67E22] hover:bg-transparent hover:text-black hover:border-[#E67E22] border"
-                >
-                  Registrarse
-                </Button>
-              </form>
-            </Form>
-          </div>
-          <div className={`${styles["form-container"]} ${styles["sign-in"]}`}>
-            <Form {...loginForm}>
-              <form
-                onSubmit={loginForm.handleSubmit(onLogin)}
-                className="flex flex-col gap-4"
-              >
-                <h1>
-                  <b>Iniciar Sesión</b>
-                </h1>
-                <FormField
-                  control={loginForm.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input placeholder="Email" {...field} />
-                      </FormControl>
-                      {loginForm.formState.errors.email && (
-                        <p className="text-red-500">
-                          {loginForm.formState.errors.email.message}
-                        </p>
-                      )}
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={loginForm.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input
-                          type="password"
-                          placeholder="Contraseña"
-                          {...field}
-                        />
-                      </FormControl>
-                      {loginForm.formState.errors.password && (
-                        <p className="text-red-500">
-                          {loginForm.formState.errors.password.message}
-                        </p>
-                      )}
-                    </FormItem>
-                  )}
-                />
-                <Button
-                  type="submit"
-                  className="w-1/2 self-center bg-transparent text-black border-[#E67E22] border hover:bg-[#E67E22] hover:text-white hover:border-none"
-                >
-                  Iniciar Sesión
-                </Button>
-              </form>
-            </Form>
-          </div>
-          <div className={styles["toggle-container"]}>
-            <div className={styles.toggle}>
-              <div
-                className={`${styles["toggle-panel"]} ${styles["toggle-left"]}`}
-              >
-                <h1>
-                  <b>¡Bienvenido de Nuevo!</b>
-                </h1>
-                <p>
-                  Ingresa tus datos personales para usar todas las funciones del
-                  sitio
-                </p>
-                <Button
-                  className={isSignUp ? "" : "hidden"}
-                  onClick={() => setIsSignUp(false)}
-                >
-                  Iniciar Sesión
-                </Button>
               </div>
-              <div
-                className={`${styles["toggle-panel"]} ${styles["toggle-right"]}`}
+              <FormField
+                control={signUpForm.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Contraseña"
+                        {...field}
+                      />
+                    </FormControl>
+                    {signUpForm.formState.errors.password && (
+                      <p className="text-red-500">
+                        {signUpForm.formState.errors.password.message}
+                      </p>
+                    )}
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={signUpForm.control}
+                name="confirm_password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Confirmar Contraseña"
+                        {...field}
+                      />
+                    </FormControl>
+                    {signUpForm.formState.errors.confirm_password && (
+                      <p className="text-red-500">
+                        {signUpForm.formState.errors.confirm_password.message}
+                      </p>
+                    )}
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="w-full bg-[#E67E22] hover:bg-transparent hover:text-black hover:border-[#E67E22] border"
               >
-                <h1>
-                  <b>¡Hola, Amigo!</b>
-                </h1>
-                <p>
-                  Regístrate con tus datos personales para usar todas las
-                  funciones del sitio
-                </p>
-                <Button
-                  className={isSignUp ? "hidden" : ""}
-                  onClick={() => setIsSignUp(true)}
-                >
-                  Registrarse
-                </Button>
-              </div>
-            </div>
-          </div>
+                Registrarse
+              </Button>
+            </form>
+          </Form>
+
+          <Form {...loginForm}>
+            <form
+              onSubmit={loginForm.handleSubmit(onLogin)}
+              className="m-10 bg-white p-10 border rounded-md shadow-md flex flex-col gap-4 items-center justify-center py-2"
+            >
+              <h1>
+                <b>Iniciar Sesión</b>
+              </h1>
+              <FormField
+                control={loginForm.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} />
+                    </FormControl>
+                    {loginForm.formState.errors.email && (
+                      <p className="text-red-500">
+                        {loginForm.formState.errors.email.message}
+                      </p>
+                    )}
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={loginForm.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Contraseña"
+                        {...field}
+                      />
+                    </FormControl>
+                    {loginForm.formState.errors.password && (
+                      <p className="text-red-500">
+                        {loginForm.formState.errors.password.message}
+                      </p>
+                    )}
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                className="w-full bg-[#E67E22] hover:bg-transparent hover:text-black hover:border-[#E67E22] border"
+              >
+                Iniciar Sesión
+              </Button>
+            </form>
+          </Form>
         </div>
-        <Link to="/empresa-cuenta" className="text-[#E67E22] hover:text-black">
+
+        <Link to="/empresa-cuenta" className="text-[#E67E22] hover:text-black mt-4">
           ¿Quieres registrar una cuenta de empresa?
         </Link>
       </div>
