cf6bce7a965a88ce3a23d61428aee2eabd724ca2
Pablo Sanchez <pablo.sanchez@dialogotecnologico.com>
2024-06-13 17:22:39 +0200

    refix: endpoint image retry

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,5 @@
 const env = {
   API_BASE_URL: "https://web-production-6e7ec.up.railway.app/api",
-  API_BASE_URL_IMG: "http://web-production-6e7ec.up.railway.app/api",
   TOKEN_KEY: "token",
   endpoints: {
     login: "/login",
